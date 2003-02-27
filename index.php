bef46280fc50722ed179c83b603a533178a20242
cbleek <cbleek>
2003-02-27 09:15:03 +0000

    increased timeout

--- index.php
+++ index.php
@@ -1,26 +1,10 @@
 <?php
 
-define('EDIT_TEXT', 1);
-
-include_once "prepend.inc";
-include_once OPENHR_LIB."/Database.php";
-include_once OPENHR_LIB."/Category.php";
-include_once "menu.inc";
-
-
-$page=&Page::singleton("categories");
-Page::fetchSlots("categories");
-Page::setSlot('menuleft',    menuleft());
-Page::setSlot('menutop',     menutop());
-Page::setSlot('menufoot',    sprintf(_("Copyright (c) 2003 %s"),"<a href=\"?content=carsten\">Carsten Bleek</a>"));
-
-$template_dir="categories/";
-
-
+#error_reporting( E_ALL );
 // SOAP TEST
 require_once("SOAP/Client.php");
 
-$ini=parse_ini_file(FILE_INI);
+$ini=parse_ini_file("/home/carsten/config/categories.conf");
 
 $endpoint     = $ini["soap.search_server"];
 
@@ -30,21 +14,18 @@ $portName     = false;
 $proxy_params = array();
 
 $sc = new SOAP_Client($endpoint);
+$sc->setErrorHandling(PEAR_ERROR_PRINT);
+
 $method  = 'get';
 $params  = array("category" => "language");
-$options = array('namespace' => 'urn:SOAP_category');
+$options = array('namespace' => 'urn:SOAP_category',
+                 'trace'     => true,
+                 'timeout'   => 10);
 $res     = $sc->call($method, $params, $options);
+
+print "<hr>".$sc->headersOut."</hr><hr>".$sc->headersIn;
+print "<pre>".htmlspecialchars($sc->__get_wire())."</pre>";
 print_r($res);
-
-$soap["request"]  ="request";
-#var_export($sc->xml,true);
-$soap["response"] ="response";
-
-$smarty->assign("daten","daten");
-$smarty->assign("SOAP",$soap);
-$smarty->assign("page",Page::getSlots());
-
-$smarty->display($template_dir.'generic.tpl');
 
 
 ?>
--- soap.php
+++ soap.php
@@ -1,13 +1,14 @@
 <?php
+define("FILE_INI", "/home/carsten/config/jobAdmin.conf");
+define("PEAR_BASE","/home/carsten/public_html/pear");
+define("OPENHR_LIB","/home/carsten/public_html/jobs/lib");
 
-require_once("prepend.inc");
 require_once('SOAP/Server.php');
 require_once(OPENHR_LIB."/Category.php");
 
 $ss = new SOAP_Server();
 
 $ss->addObjectMap(new SOAP_category, 'urn:SOAP_category');
-
 $ss->service($HTTP_RAW_POST_DATA);
 
 class SOAP_category{
@@ -20,7 +21,6 @@ class SOAP_category{
         $cat=new Category;
         return $cat->getChilds("language");
     }
-
 }
 
 
